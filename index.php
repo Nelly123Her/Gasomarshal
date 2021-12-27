2ea1e93eab8009329a3418b9fc44b783907d0901
NelliePotato27 <nelly.hernandez@hiumanlab.com>
2021-12-26 18:17:43 -0600

    change version

--- index.php
+++ index.php
@@ -1,1 +1,326 @@
-echo "Hello, World!";
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title></title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <!-- Google Fonts -->
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <!--Importar los iconos de las redes sociales-->
+
+
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+  <!-- ======= Header ======= -->
+  <!--En esta sección se crea el header con la etiqueta header, y un contenedor donde estarán los
+    items (navs) donde nos redigiran alrededor de la páigna-->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo"><img src="logo.png" alt=""></a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <!--En este apartado se crean los navlinks con listas no ordenadas
+            con etiqueta de hipervinculo (a), con la clase nav-link scrollto active en la primera sección 
+          home -->
+
+          <li><a class="nav-link scrollto active" href="#header">Home</a></li>
+          <li><a class="nav-link scrollto" href="#">Facturación</a></li>
+          <li><a class="nav-link scrollto" href="#">Clientes</a></li>
+          <li><a class="nav-link scrollto " href="#">Software</a></li>
+          <li><a class="nav-link scrollto" href="#services">Hardware</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contáctanos</a></li>
+        </ul>
+
+        <!--Logo del navbar móvil o hamburguesa-->
+
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+
+  <!--En esta sección se crea el hero, junto con el texto que cuenta con la sección-->
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container" data-aos="zoom-out" data-aos-delay="100">
+      <br>
+      <br>
+      <br><br>
+      <br><br>
+      <h3>Bienvenido a Gasomarshal</h3>
+      <h1>Especializados en el <br>
+        desarrollo e integración de <br> tecnologías y servicios <br> para el sector gasolinero.</h1>
+              
+      <div class="btn-facturacion">
+        <h4>Facture aquí</h4>
+      </div>
+
+    </div>
+  </section><!-- End Hero -->
+  <main id="main">
+
+    <!--Services section-->
+    <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+        <img src="tanques.png" alt="" srcset="">
+
+        <div class="section-title">
+          <h2>Kioscos y Pedestales y Puntos de Venta</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 col-md-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="100">
+            <div class="icon-box">
+              <p>
+                Muebles y equipos esenciales para servicio a clientes en áreas de despacho.</p>
+              <img src="assets/img/hardware/1.png" class="img-fluid" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 d-flex align-items-center mt-4 mt-md-0" data-aos="fade-up-left"
+            data-aos-delay="200">
+            <div class="icon-box">
+              <p>Equipos que incluyen impresora, pantalla táctil, y lector para QR®, NFC®, tarjetas.</p>
+              <img src="assets/img/hardware/puntodeventa.png" class="img-fluid" alt="">
+
+            </div>
+          </div>
+        </div>
+        <div class="section-title">
+          <h2>Dispensadores y Refacciones</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 col-md-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <p>
+                Muebles y equipos esenciales para servicio a clientes en áreas de despacho.</p>
+              <img src="assets/img/hardware/dispensador.png" class="img-fluid" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 d-flex align-items-center mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <p>Equipos que incluyen impresora, pantalla táctil, y lector para QR®, NFC®, tarjetas.</p>
+              <img src="assets/img/hardware/refacciones.png" class="img-fluid" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="section-title">
+          <h2>Servidores e Interfaz para Dispensarios</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 col-md-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <p>
+                Muebles y equipos esenciales para servicio a clientes en áreas de despacho.</p>
+              <img src="assets/img/hardware/servidor-png.png" class="img-fluid" alt="">
+
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-md-6 d-flex align-items-center mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <p>Equipos que incluyen impresora, pantalla táctil, y lector para QR®, NFC®, tarjetas.</p>
+              <img src="assets/img/hardware/interfaz-2.png" class="img-fluid" alt="">
+
+            </div>
+          </div>
+        </div>
+        <div class="section-title">
+          <h2>Preciadores Electrónicos y Consola para Tanques</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 col-md-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <p>
+                Muebles y equipos esenciales para servicio a clientes en áreas de despacho.</p>
+              <img src="assets/img/hardware/preciadores.png" class="img-fluid" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 d-flex align-items-center mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <p>Equipos que incluyen impresora, pantalla táctil, y lector para QR®, NFC®, tarjetas.</p>
+              <img src="assets/img/hardware/consola-tanques.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+        </div>
+    </section><!-- End Services Section -->
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title-contact">
+          <h2>Apreciamos su contacto </h2>
+        </div>
+
+        <div class="row" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-lg-6">
+            <div class="info-box mb-4">
+              <i class="bx bx-map"></i>
+              <h3>Nuestra dirección </h3>
+              <p>A108 Adam Street, New York, NY 535022</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="info-box  mb-4">
+              <i class="bx bx-envelope"></i>
+              <h3>Email </h3>
+              <p> ventas@gasomarshal.com</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="info-box  mb-4">
+              <i class="bx bx-phone-call"></i>
+              <h3>Llámanos</h3>
+              <p>+1 999 999 9999</p>
+            </div>
+          </div>
+        </div>
+        <div class="row" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="col-lg-6 ">
+            <iframe class="mb-4 mb-lg-0"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14894.721988292355!2d-89.6411268!3d21.0454664!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x955b0723ff5e7d2b!2sGasomarshal%20Control%20y%20Seguridad%20Inteligente!5e0!3m2!1ses-419!2smx!4v1640548414189!5m2!1ses-419!2smx"
+              frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+          </div>
+
+          <div class="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Nombre" required>
+                </div>
+                <div class="col form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="tel" class="form-control" name="telephone" id="telephone" placeholder="Telefóno" required>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" name="message" rows="5" placeholder="Déjanos tu mensaje"
+                  required></textarea>
+              </div>
+
+
+              <!--
+                 <div class="my-3">
+                <div class="loading">Cargando</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Tu mensaje ha sido enviado. ¡Gracias!</div>
+              </div>
+              -->
+
+              <div class="text-center"><button type="submit">Enviar mensaje</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-lg-3 col-md-6 footer-contact">
+              <a href="index.html" class="logo"><img src="logo.png" alt="" width="200px"></a>
+              <p>
+                Dirreción <br>
+                <strong>Telefóno:</strong> +52 (999) 195 2497<br>
+                <strong>Email:</strong> atencionaclientes@gasomarshal.com<br>
+              </p>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+              <!-- <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul> 
+          -->
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+              <h4>Nuestros servicios</h4>
+              <ul>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Facturación</a></li>
+              </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+              <h4>Redes sociales</h4>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="youtube"><i class="bx bxl-youtube"></i></a>
+
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="container py-4">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Gasomarshal</span></strong>. Todos los derechos reservados
+        </div>
+        <div class="credits">
+          Diseñador por <a href="">Eduardo Romero Mucel</a>
+        </div>
+      </div>
+    </footer><!-- End Footer -->
+
+    <div id="preloader"></div>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
